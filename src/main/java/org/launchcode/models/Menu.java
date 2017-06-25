ecdf5e262afa8f045d71a953eddf71b44aef8463
Lynn Strauss <lmswoboda@fandbfin.com>
2017-06-25 18:33:57 -0500

    close

---
+++ src/main/java/org/launchcode/models/Menu.java
@@ -0,0 +1,53 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+/**
+ * Created by lynnstrauss on 6/25/17.
+ */
+@Entity
+public class Menu {
+
+    @NotNull
+    @Size(min=3, max =15)
+    private String name;
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @ManyToMany
+    private List<Cheese> cheeses;
+
+    public Menu() {};
+
+    public Menu(String name) {
+        this.name = name;
+    }
+
+    public void addItem(Cheese item){
+        cheeses.add(item);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Cheese> getCheeses() {
+        return cheeses;
+    }
+}
